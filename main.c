ccc144e8764d07eab9309f23bad399e46ddfeba7
Vaibhav Gupta <vaibhavgupta.avy1013@gmail.com>
2023-10-17 00:31:55 +0530

    Program for demotion and promotion
    Its a program that shows the demotion of value of float to int

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
  #include<stdio.h>
+ int conversion() { //this is new funtion for understanding how does the promotion and demotion of int and float value works
+    float nj=3.4;
+    int i;
+    i= nj*1;
+    printf("%d\n",i); 
+    return 0;
+}
  int main(){
     int adp=1+1+18+27;          //just power fun way to calculate
     printf("Hello world \n%d\n",adp); // hello world 
+    conversion(); // hurray!😀 i called the function  
     return 0;
  }
