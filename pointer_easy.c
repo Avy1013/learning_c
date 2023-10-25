6c1e58b8b2b4d79d9d9a3abb79129d9ba01698b1
Vaibhav Gupta <vaibhavgupta.avy1013@gmail.com>
2023-10-26 04:19:32 +0530

    Updated call by reference concept
    It basically leverages the power of pointers as it now passes value of addresses from one function and catches that value in another function making it possible hope🤞 it makes sense

--- pointer_easy.c
+++ pointer_easy.c
@@ -1,14 +1,29 @@
 
-#include <stdio.h>
-// *pointer means value at address
+// * pointer means value at address
 // whereas & means value of address
+
+#include <stdio.h>
+void areaperi(int r,float *a,float *p){
+    *a=3.14*r*r;
+    *p=2*3.14*r;
+}
+void call_by_reference(){ //as we know return statement cannot return two value pointers solve it
+    int radius;
+    float area,perimeter;
+    printf("enter the radius: ");
+    scanf("%d",&radius);
+    areaperi(radius,&area,&perimeter);
+    printf("area= %f",area);
+    printf("\nperimeter= %f\n",perimeter);
+}
 
 int main() {
     int i=3,*j,**k;
     j = &i;
     k = &j;
-    printf("%p\n",k);
-    printf("%p\n",&j);
-    printf("%p\n",*k);
-    printf("%p\n",&i);
+    printf("Address of j: %p\n",k);
+    printf("Address of j: %p\n",&j);
+    printf("Address of i(As '*' means value at address): %p\n",*k);
+    printf("Address of i: %p\n",&i);
+    call_by_reference();
 }
