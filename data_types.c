c4bf2e9b6e34d97e6e45c74e8d5ce94d97509b5b
Vaibhav Gupta <vaibhavgupta.avy1013@gmail.com>
2023-10-28 17:28:59 +0530

    info_Data_types

---
+++ data_types.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include <unistd.h> // Include the <unistd.h> header for sleep
+#include"image.h"
+// this represents 
+int main(){
+
+    image("images../different_types_of_storage_class.jpeg");
+    printf("hhhh\n");
+    image("images../Data_types.jpg");
+    
+    return 0;
+}
