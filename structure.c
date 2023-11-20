aac90505b62dbab37116a6a3d691f532539424b1
Vaibhav Gupta <vaibhavgupta.avy1013@gmail.com>
2023-11-21 01:15:27 +0530

    arrow operator
    Passing of structure

--- structure.c
+++ structure.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
-int main() {
-    struct book {
-        char name;  // Using an array for the book name
+struct book {
+        char name;  
         float price;
         int pages;
     };
+void display(struct book *poi){
+    printf("%c %f %d\n",poi->name,poi->price,poi->pages);
+}
+int main() {
+    
     
     struct book b[10];
-    for (size_t i = 0; i < 3; i++)
+    for (size_t i = 0; i < 2; i++)
     {
        printf("Enter names, price, and pages of 3 books:\n");
        scanf(" %c %f %d", &b[i].name, &b[i].price, &b[i].pages);
     }
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < 2; i++)
     {
         printf("Book %d: %c, %f, %d\n",i, b[i].name, b[i].price, b[i].pages);
-    }
+    }    
+    display(&b[0]);
     return 0;
 }
