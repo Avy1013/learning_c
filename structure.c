a3358850a7d165b32bfc6330f0b129c518151bef
Vaibhav Gupta <vaibhavgupta.avy1013@gmail.com>
2023-11-21 00:06:43 +0530

    Using array for structure
    This way prevents us retyping everything

--- structure.c
+++ structure.c
@@ -6,22 +6,15 @@ int main() {
         int pages;
     };
     
-    struct book b1, b2, b3;
-    
-    printf("Enter names, price, and pages of 3 books:\n");
-    
-    // Input for book 1
-    scanf(" %c %f %d", &b1.name, &b1.price, &b1.pages);
-    
-    // Input for book 2
-    scanf(" %c %f %d", &b2.name, &b2.price, &b2.pages);
-    
-    // Input for book 3
-    scanf(" %c %f %d", &b3.name, &b3.price, &b3.pages);
-    
-    printf("Book 1: %c, %.2f, %d\n", b1.name, b1.price, b1.pages);
-    printf("Book 2: %c, %.2f, %d\n", b2.name, b2.price, b2.pages);
-    printf("Book 3: %c, %.2f, %d\n", b3.name, b3.price, b3.pages);
-
+    struct book b[10];
+    for (size_t i = 0; i < 3; i++)
+    {
+       printf("Enter names, price, and pages of 3 books:\n");
+       scanf(" %c %f %d", &b[i].name, &b[i].price, &b[i].pages);
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        printf("Book %d: %c, %f, %d\n",i, b[i].name, b[i].price, b[i].pages);
+    }
     return 0;
 }
